ad615809718e139d51be7a61f60928840e37d39c
ronniekram <arynn.boniface@gmail.com>
2023-07-01 11:51:52 -0400

    working: update next config to fix serverless function size

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,16 @@ const withTwin = require("./with-twin");
 
 const nextConfig = {
   reactStrictMode: true,
-}
+  legacyBrowsers: false,
+    outputFileTracingRoot: path.join(__dirname, `../../`),
+    outputFileTracingExcludes: {
+      "*": [
+        // prettier-ignore
+        `node_modules/@swc/core-linux-x64-gnu`,
+        `node_modules/@swc/core-linux-x64-musl`,
+        `node_modules/@esbuild/linux-x64`,
+      ],
+    },
+};
 
 module.exports = withTwin(nextConfig);
